bab135a5dbcb6788d33fa7fd68612754618f3f34
Bektur13 <duyshembekov04@mail.ru>
2026-05-24 22:04:18 -0700

    Implement GET http method.

--- src/routes/applications.ts
+++ src/routes/applications.ts
@@ -1,7 +1,12 @@
-// const express = require('express');
-// const app = express();
+import express from 'express';
 
+const app = express();
+const port = 3000;
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World')
-// });
+app.get('/applications', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+})
