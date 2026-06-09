e8c12b13e06ab1ea852ae0b7b15fb4fd99c6fded
Bektur13 <duyshembekov04@mail.ru>
2026-06-08 19:34:18 -0700

    Add CRUD POST route for application in prisma

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,6 +1,35 @@
-import { PrismaClient } from '@/prisma/client';
+import { prisma } from '@/lib/prisma';
 
 
-export const prisma = new PrismaClient();
+// model Application {
+//   id Int  @id @default(autoincrement())
+//   company  String 
+//   role  String
+//   status  String  @default("applied")
+//   date_applied  DateTime  @default(now())
+//   notes String?
+//   skills  String[]
+// }
 
-const 
+const create = async () => {
+    const application = await prisma.applications.create({
+        data: {
+            company: "Google",
+            role: "Software Engineer",
+            notes: "Applied via referral",
+            id: 1, 
+            status: "PENDING",  
+            date_applied: new Date(),
+        }
+    });
+    console.log("Created application: ", application)
+
+}
+
+create().then(async () => {
+    await prisma.$disconnect();
+}).catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+})
