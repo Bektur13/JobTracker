342eaad5021b8f49ddebec6a9117a0dcceeb446e
Bektur13 <duyshembekov04@mail.ru>
2026-06-09 17:38:00 -0700

    Implement GET + POST routes with PrismaClient

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import express from 'express';
-import { }
+
+import applicationsRoutes from '@/routes/applications';
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());
 
-app.get('/routes/applications.ts', (req, res) => {
-    res.send('GET method!');
+app.use('/applications', applicationsRoutes);
+
+app.use((err: Error, _req: express.Request, res: express.Response, _next: express.NextFunction) => {
+    console.error('Express error handler:', err);
+    res.status(500).json({ error: err.message });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
 });
--- src/routes/applications.ts
+++ src/routes/applications.ts
@@ -1,16 +1,25 @@
-import { Request, Response } from 'express';
+import { Request, Response, Router } from 'express';
 import { prisma } from '@/lib/prisma';
 
-const create = async (req: Request, res: Response) => {
+const router = Router();
+
+router.post('/', async (req: Request, res: Response) => {
     try {
         const application = await prisma.application.create({ data: req.body });
-
         return res.status(201).json(application);
-    } catch (error) {
-        return res.status(500).json({ error: "Unable to create application"})
+    } catch {
+        return res.status(500).json({ error: "Unable to create application" });
     }
-}
+});
 
-const get = async () => {
-    const applicaton = await prisma.application.findMany();
-}
+router.get('/', async (req: Request, res: Response) => {
+    try {
+        const application = await prisma.application.findMany();
+        return res.status(200).json(application);
+    } catch (error) {
+        console.error('GET error:', error);
+        return res.status(500).json({ error: "Unable to fetch application" });
+    }
+});
+
+export default router;
