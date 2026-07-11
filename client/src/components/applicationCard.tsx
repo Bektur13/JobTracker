f676512c83abc9f7b9bb70b4ce2a2399a91f1c4a
Bektur13 <duyshembekov04@mail.ru>
2026-07-10 22:46:22 -0700

    Create client side using Vite and React. Implement ApplicationCard component

---
+++ client/src/components/applicationCard.tsx
@@ -0,0 +1,10 @@
+
+export const ApplicationCard = (company: string, role: string, status: string) => {
+    return (
+        <ul>
+            <li>Google, Software Engineer, applied</li>
+            <li>Amazon, Software Engineer, interviewing</li>
+            <li>Netflix, Software Engineer, rejected</li>
+        </ul>
+    )
+}
