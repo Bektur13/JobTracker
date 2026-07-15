00fe3f176bb1c60c336b2520c3bc5984f1de8727
Bektur13 <duyshembekov04@mail.ru>
2026-07-14 23:14:48 -0700

    Implement POST route handler

--- src/app/api/applications/route.ts
+++ src/app/api/applications/route.ts
@@ -1,3 +1,25 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(res)
+export async function POST(request: NextRequest) {
+    try {
+        const body = await request.json();
+        const { company, role, status, notes, skills} = body;
+        if(!company || !role || !status || !notes || !skills) {
+            return NextResponse.json(
+                { error: 'Missing required fields'},
+                { status: 400 }
+            )
+        }
+
+        return NextResponse.json(
+            { 
+                message: 'Application created successfulluy!',
+                data: {company, role, status, notes, skills
+            }}
+        )
+    }catch(e) {
+        return NextResponse.json({
+            error: "Invalid JSON format"
+        }, { status: 400 })
+    }
+}
