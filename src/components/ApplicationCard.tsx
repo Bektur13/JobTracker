b283e95deb6452e26c528bda1d9b8e922f082f16
Bektur13 <duyshembekov04@mail.ru>
2026-07-16 23:47:59 -0700

    Working on ApplicationForm component. Need to fix form component

--- src/components/ApplicationCard.tsx
+++ src/components/ApplicationCard.tsx
@@ -1,7 +1,12 @@
 'use client'
 
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useState, useEffect} from 'react';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
 
+import { Button } from '@/components/ui/button';
+import { Form } from 'react-hook-form';
 
 export const ApplicationCard = () => {
     return (
