4b0ca7265f462488449afac198e5accd75325e69
Bektur13 <duyshembekov04@mail.ru>
2026-08-07 10:54:49 -0700

    Install Clerk. Add env clerk variables

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,15 +1,15 @@
-import { prisma } from '@/lib/prisma';
+// import { prisma } from '@/lib/prisma';
 
-const create = async () => {
-    const application = await prisma.jobApplication.create({});
-    console.log("Created application: ", application)
+// const create = async () => {
+//     const application = await prisma.jobApplication.create({});
+//     console.log("Created application: ", application)
 
-}
+// }
 
-create().then(async () => {
-    await prisma.$disconnect();
-}).catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-})
+// create().then(async () => {
+//     await prisma.$disconnect();
+// }).catch(async (e) => {
+//     console.error(e);
+//     await prisma.$disconnect();
+//     process.exit(1);
+// })
