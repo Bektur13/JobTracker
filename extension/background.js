d4c232b5e9b551817ddb7b16d99782610c8ea104
Bektur13 <duyshembekov04@mail.ru>
2026-08-25 16:31:21 -0700

    Implement tabs permission

---
+++ extension/background.js
@@ -0,0 +1,5 @@
+// MV3 service worker for CareerTrack Job Clipper.
+// TODO (next step): listen for a "start-connect" message from the popup,
+// open /extension/connect in a new tab, watch for it to redirect to
+// /extension/connected?code=..., then POST that code to
+// /api/extension/exchange to retrieve and store the API key.
